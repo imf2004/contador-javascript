ebfe8186f0a3b8f0312fef871c71e9873f77bf17
imf2004 <imoraes.forte@gmail.com>
2022-01-16 23:02:01 -0300

    Contador de números atualizado

---
+++ assets/js/javascript.js
@@ -0,0 +1,44 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var buttonMore = document.getElementById("more");
+var buttonLess = document.getElementById("less");
+var currentNumber = 0;
+
+function increment() {
+  currentNumber = currentNumber + 1
+  currentNumberWrapper.innerHTML = currentNumber
+  validateColor()
+  buttonMore.disabled = false
+  buttonMore.classList.remove("cinza")
+  if (currentNumber == 10) {
+    buttonMore.disabled = true
+    buttonMore.classList.add("cinza")
+  }
+  if (currentNumber > -10) {
+    buttonLess.disabled = false
+    buttonLess.classList.remove("cinza")
+  }
+}
+
+function decrement() {
+  currentNumber = currentNumber - 1;
+  currentNumberWrapper.innerHTML = currentNumber;
+  validateColor()
+  buttonLess.disabled = false
+  buttonLess.classList.remove("cinza")
+  if (currentNumber == -10) {
+    buttonLess.disabled = true
+    buttonLess.classList.add("cinza")
+  }
+  if (currentNumber < 10) {
+    buttonMore.disabled = false
+    buttonMore.classList.remove("cinza")
+  }
+}
+
+function validateColor () {
+  if (currentNumber  < 0) {
+    currentNumberWrapper.style.color = "#ff0000"
+  } else {
+    currentNumberWrapper.style.color = "#000000"
+  } 
+}
